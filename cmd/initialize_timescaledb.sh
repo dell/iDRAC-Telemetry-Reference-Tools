e5d6212e118ae8dcbc0b45524f58e4c06e498a5b
michael_e_brown <michael_e_brown@dell.com>
2022-02-26 00:36:16 -0600

    Add timescale profiles
    Add test timescale database, add configuration for timescale.
    Add timescale-pump for both standalone and test databases

--- cmd/initialize_timescaledb.sh
+++ cmd/initialize_timescaledb.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+
+set -x
+set -e
+
+export PGPASSFILE=/tmp/.pgpass
+touch $PGPASSFILE
+chmod 600 $PGPASSFILE
+echo "*:*:*:$POSTGRES_USER:$POSTGRES_PASS" > $PGPASSFILE
+
 sleep 10
-psql --username "$POSTGRES_USER" <<EOF
-CREATE DATABASE $TIMESCALE_DB WITH OWNER $POSTGRES_USER;
-GRANT ALL PRIVILEGES ON DATABASE $TIMESCALE_DB TO $POSTGRES_USER;
-EOF
+
+echo "create telemetry database if it does not exist"
+echo "
+  SELECT
+    'CREATE DATABASE $TIMESCALE_DB WITH OWNER $POSTGRES_USER;' |
+    'GRANT ALL PRIVILEGES ON DATABASE $TIMESCALE_DB TO $POSTGRES_USER;'
+  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = $TIMESCALE_DB)\gexec" |
+  psql -h $POSTGRES_HOST --port $POSTGRES_PORT --username postgres <<EOF
