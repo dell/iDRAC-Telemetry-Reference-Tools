1d52968931b723c58bf38432dc28788936523ef7
Sankara Gara <sankara_gara@dell.com>
2022-07-28 18:30:28 -0500

    TLM-591 :  Fix timescale DB creation and connection issues

--- cmd/timescalepump/initialize_timescaledb.sh
+++ cmd/timescalepump/initialize_timescaledb.sh
@@ -11,9 +11,14 @@ echo "*:*:*:$POSTGRES_USER:$POSTGRES_PASS" > $PGPASSFILE
 sleep 10
 
 echo "create telemetry database if it does not exist"
-echo "
-  SELECT
-    'CREATE DATABASE $TIMESCALE_DB WITH OWNER $POSTGRES_USER;' |
-    'GRANT ALL PRIVILEGES ON DATABASE $TIMESCALE_DB TO $POSTGRES_USER;'
-  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = $TIMESCALE_DB)\gexec" |
-  psql -h $POSTGRES_HOST --port $POSTGRES_PORT --username postgres <<EOF
+
+psql -h $POSTGRES_HOST --port $POSTGRES_PORT --username postgres << EOF
+CREATE DATABASE $TIMESCALE_DB WITH OWNER $POSTGRES_USER;
+GRANT ALL PRIVILEGES ON DATABASE $TIMESCALE_DB TO $POSTGRES_USER;
+EOF
+
+# Check if DB created/exists  (debug purpose only)
+psql -h $POSTGRES_HOST --port $POSTGRES_PORT --username postgres << EOF
+\l
+EOF
+
