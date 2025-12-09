32967afdfb327b1e2a3a00be9099e23a98e69490
nikita_goyal <nikita_goyal@dell.com>
2025-12-09 11:20:42 -0600

    added in package auth.go type ValveState

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -47,7 +47,7 @@ type Service = auth.Service
 type ServiceItem = auth.ServiceItem
 type Command = auth.Command
 type SplunkConfig = auth.SplunkConfig
-type Valvestate = auth.ValveState
+type ValveState = auth.ValveState
 
 type AuthorizationClient = auth.AuthorizationClient
 type AuthorizationService = auth.AuthorizationService
